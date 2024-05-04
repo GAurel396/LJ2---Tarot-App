9a988646f96631cc84bbd97f6fb6c99b3c5c2246
Aurel <157985436+GAurel396@users.noreply.github.com>
2024-05-04 18:46:35 +0200

    fuck all

---
+++ script.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector(".game_canvas");
+const ctx = canvas.getContext("2d");
+//selecting body to implement pause on click-out
+const body = document.querySelector("body")
+
