3ff1286c22e0968239dce068957aad9a9243acbd
Aurel <157985436+GAurel396@users.noreply.github.com>
2024-05-06 14:14:17 +0200

    reorganized and added fonts

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const ctx = canvas.getContext("2d");
 //selecting body to implement pause on click-out
 const body = document.querySelector("body")
 
+function init () {
+
+}
+
+function draw_title () {
+
+}
+
+function draw_menu () {
+
+}
+
+function shuffle () {
+    
+}
