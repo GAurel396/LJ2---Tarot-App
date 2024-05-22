d1ed1256d20070bc500c6c67f1a8ce768d52be99
Aurel <157985436+GAurel396@users.noreply.github.com>
2024-05-23 00:57:54 +0200

    about works

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ logo.src = "/graphics/splash-screen-sheet.png"
 logo.classList.add("logo");
 //Title screen
 const title = new Image();
-title.src = "/graphics/TITLEplaceholder.png";
+title.src = "graphics/title.png"
 title.classList.add("title_screen");
 //menu
 const menu = new Image();
@@ -45,10 +45,13 @@ function draw_title() {
         console.log("Title is called");
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(title, 0, 0, 255, 223);
+        ctx.font =  "12px bm_japan";
+        ctx.textAlign  = "center";
+        ctx.fillText("Press Start", 178, 113);
         state = "title";
         addEventListener("keydown", function (e) {
             console.log("which key was press: " + e.key);
-            if (state == "title" && e.key === "Enter") {
+            if (state === "title" && e.key === "Enter") {
                 draw_menu();
             }
         });
@@ -84,14 +87,14 @@ let size_y;
 let selector_radius;
 
 function draw_rectangle() {
-        if (menu_option == 1) {
+        if (menu_option === 1) {
                 selector_x = 45;
                 selector_y = 65;
                 size_x = 62;
                 size_y = 94;
                 selector_radius = 3;
                 ctx.clearRect(132, 53, 97, 126);
-        } else if (menu_option == 2) {
+        } else if (menu_option === 2) {
                 selector_x = 134;
                 selector_y = 55;
                 size_x = 89;
@@ -111,11 +114,11 @@ function draw_rectangle() {
 
 window.addEventListener("keydown", function (e) {
         console.log(e.key);
-        if (state == "menu") {
-                if (e.key == "ArrowLeft" || e.key == "ArrowRight") {
-                        if (menu_option == 1) {
+        if (state === "menu") {
+                if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+                        if (menu_option === 1) {
                                 menu_option = 2;
-                        } else if (menu_option == 2) {
+                        } else if (menu_option === 2) {
                                 menu_option = 1;
                         }
                 }
@@ -132,42 +135,91 @@ let l_page_x = 26;
 let l_page_y = 22;
 let r_page_x = 137;
 let r_page_y = 22;
-let left_page_test = `Tarot is many thing to many people. Cartomancy. Way to delve into own depths. Way to connect
-                        with universe or your deities. Method to pick what to eat. It can be any of that. Secular or spiritual.
-                        (But please don't sue us even if you visit us for divination!) 
-                        A basic method is to have a question, focus on the question, shuffle the deck and interpret the card you see to answer the question.
-                        Focus on the elements in image, focus on the feeling, focus on the words of description. Advanced learners may also focus on associated elements or even use assigned hebrew
-                        letters as springboard into  Qaballa. Still, even focusing on the answer Tarot gives us instinctually is useful. IN fact, listening to what youa re feeling looking at the card and its description is the most powerful tool in yoiur arsenal`
 
-function switch_about() {
-        console.log("only console log for now - about");
+let page1 = new Image();
+page1.src = "graphics/ABOUTpage1rt.png"
+
+let page2 = new Image();
+page2.src = "graphics/ABOUTpage2t.png"
+
+let page4 = new Image();
+page4.src = "graphics/aboutpage3.png";
+
+let example = new Image();
+example.src = "graphics/cards/0.png";
+
+let pageleft = page1;
+let pageright = page2; 
+let plx = 92;
+let ply = 151;
+let book_state = 1;
+
+function change_book_state (){
+        if (book_state === 1) {
+                pageleft = page1;
+                pageright = page2;
+                plx = 92; 
+                ply = 151;
+        } else {
+                pageleft = example;
+                pageright = page4;
+                plx = 96; 
+                ply = 128;
+        }
+        switch_about();
+};
+
+function switch_about() {;
         state = "about";
         console.log(state);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(about_screen, sx, sy, sw, sh, 0, 0, 256, 224);
-        ctx.font = "12px bm-japan";
-        ctx.fillStyle = " #000000 ";
-        ctx.textAlign = "left"
-        ctx.fillText("TAROT 101", 37,32);
+        ctx.drawImage(pageleft, 26, 22, plx, ply);
+        ctx.drawImage(pageright, 137,22, 92, 151);
 };
 
+
+window.addEventListener("keydown", function (e) {
+        if (state === "about") {
+                if (e.key === "ArrowRight" && book_state < 2) {
+                        console.log("key pressed")
+                        book_state += 1;
+                        console.log(book_state);
+                        change_book_state();
+                } else if (e.key === "ArrowLeft" && book_state > 1) {
+                        console.log("left pressed");
+                      book_state -= 1;    
+                      console.log (book_state);
+                      change_book_state();       
+                }
+        }
+
+}) 
+
+let shuffle_menu = new Image();
+shuffle_menu.src = "graphics/shuffling_screesnng.png"; 
+let shuffling = new Image();
+shuffling.src = "graphics/shufflin_screen.png";
+
+
 function switch_shuffle_menu() {
-        console.log("only console log for now - shuffle page")
-        //state = "shuffling"
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(shuffle_menu, 0,0, 256, 224)
+        ctx.font = "12px BM_Japan"
+        ctx.FIllText("Press A to shuffle.")
 
 
 };
 window.addEventListener("keydown", function (e) {
-        if (state == "about" && e.key == "x") { 
+        if (state === "about" && e.key === "x") { 
                 ctx.clearRect(0,0, canvas.width, canvas.height);
                 draw_menu();
         }
 });
 window.addEventListener("keydown", function (e) {
-        if (state == "menu") {
-        if (e.key == "y" || e.key == "z" || e.key == " ") {
-                if (menu_option == 1) {
+        if (state === "menu") {
+        if (e.key === "y" || e.key === "z" || e.key === " ") {
+                if (menu_option === 1) {
                         switch_shuffle_menu();
                 } else {
                         switch_about()
@@ -176,9 +228,6 @@ window.addEventListener("keydown", function (e) {
         }
 })
 
-const body = document.querySelector("body");
-const deck_board = new Image();
-deck_board.src = "graphics/CARDDECKPLACEMENT.png"
 
 let tarot_array = [];
 for (let i = 1; i <= 22; i++) {
@@ -193,7 +242,11 @@ function shuffle() {
                 }
                 return array;
         };
-        ctx.font = "12px, font_04b03";
+        ctx.font = "9px bm_space";
+        ctx.fillStyle = " #000000 ";
+        ctx.textAlign = "left";
+        ctx.fillText(left_page_test, 37, 47);
+
         shuffle_deck(tarot_array);
         return (tarot_array[0]);
 };
@@ -203,13 +256,6 @@ function deal_card() {
         let drawn_card = tarot_array[0];
         console.log(drawn_card);
 
-}
-deck_board.onload = () => {
-        ctx.drawImage(deck_board, 0, 0, 256, 224),
-                ctx.fillStyle = "#800034 ";
-        ctx.roundRect(96, 64, 64, 96, 5);
-        ctx.fill();
-        ctx.stroke();
 }
 
 function draw_explanation (drawn_card) {
